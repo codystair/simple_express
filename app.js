02a4f96848e2cf28c0bb36bb13aef8c19c818a5f
Cody Stair <ccstair27@gmail.com>
2020-09-25 17:06:13 -0400

    Start requestbin rehaul

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://127.0.0.1:27017';
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const gitWebhookHandler = async (req, res) => {
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    const db = client.db('payloads');
+    const collection = db.collection('commits');
+    await collection.insertOne(req.body);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+    res.sendStatus(200);
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('It works!')
