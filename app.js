0b70b1458819fbb5bb4a147865356b5b0cb89c1c
Cody Stair <ccstair27@gmail.com>
2020-09-25 20:13:27 -0400

    Add PG db

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { MongoClient } = require('mongodb');
 const url = 'mongodb://127.0.0.1:27017';
 const { Client } = require('pg');
 const pgdb = new Client({
-  database: 'requestbin'
+  database: 'bins'
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +20,11 @@ const gitWebhookHandler = async (req, res) => {
     const db = client.db('payloads');
     const collection = db.collection('commits');
     const data = await collection.insertOne(req.body);
-    const id = data.insertedId;
+    const id = Number(data.insertedId);
+    
+    await pgdb.connect();
+    await pgdb.query(`INSERT INTO events (doc_id) VALUES (${id});`);
+    await pgdb.end();
   } catch (e) {
     console.error(e);
   } finally {
